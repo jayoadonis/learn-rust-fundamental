2addeea713c4a9902891ecd0e5dbb016b95f2f0e
jayoadonis <jayo.adonisraphael@gmail.com>
2024-05-27 15:17:22 +0800

    init: 0.0.0, learn-rust-fundamental

---
+++ conditional-statement/src/main/main_exe.rs
@@ -0,0 +1,9 @@
+
+
+#![allow(unused)]
+#![allow(dead_code)]
+
+pub fn main() -> Result<(), usize> {
+    println!("Fundamental Conditional statement.");
+    return Ok(());
+}
---
+++ data-type/src/main/main_exe.rs
@@ -0,0 +1,8 @@
+
+#![allow(unused)]
+#![allow(dead_code)]
+
+pub fn main() -> Result<(), usize> {
+    println!("Fundamental Data type.");
+    return Ok(());
+}
