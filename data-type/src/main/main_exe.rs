11d15e44177e355efa13f36094fcbd24a040c100
jayoadonis <jayo.adonisraphael@gmail.com>
2024-05-27 16:35:37 +0800

    chore: 0.0.1, start data-type tut

--- data-type/src/main/main_exe.rs
+++ data-type/src/main/main_exe.rs
@@ -1,8 +1,72 @@
 
 #![allow(unused)]
 #![allow(dead_code)]
+#![allow(non_snake_case)]
+
+use std::{ffi::c_uchar, sync::atomic};
 
 pub fn main() -> Result<(), usize> {
     println!("Fundamental Data type.");
+
+    //REM: PREMITIVE DATA TYPE
+    let boolean: bool;              //REM: 1 byte (8 bits) 
+                                    //REM: but only the least significant bit set to 1 representing TRUE, otherwise set all to 0 representing FALSE
+                                    //REM: 0 to 1
+    
+    let char32Bit: char;            //REM: In Rust char have 4 bytes (32 bits)
+                                    //REM: Unicode Scalar Value
+                                    //REM: 0 to (0x10FFFF or 1_114_111)
+
+    let byte: i8;                   //REM: 1 byte (8 bits)            
+                                    //REM: -128 to +127
+
+    let unsignedByte: u8;           //REM: unsigned 1 byte (8 bits)   
+                                    //REM: 0 to 255
+
+    let short: i16;                 //REM: 2 bytes (16 bits)        
+                                    //REM: -32_768 to 32_767
+    
+    let unsignedShort: i16;         //REM: unsigned 2 bytes (16 bits)
+                                    //REM: 0 to 65_535
+
+    let integer32Bit: i32;          //REM: integer (4 bytes)
+                                    //REM: -2_147_483_648 to 2_147_483_647
+                                    //REM: BILLION
+
+    let unsignedInteger32Bit: u32;  //REM: unsigned integer (4 bytes)
+                                    //REM: 0 to 4_294_967_295
+                                    //REM: BILLION
+
+    let integer64Bit: i64;          //REM: long (8 bytes)
+                                    //REM: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+                                    //REM: QUINTILLION
+
+    let unsignedInteger64Bit: u64;  //REM: unsigned long (8 bytes)
+                                    //REM: 0 to 18,446,744,073,709,551,615
+                                    //REM: QUINTILLION
+
+    let float32Bit: f32;            //REM: float (6 to 7 precision) (4 bytes)
+                                    //REM: ~1.2E-38 to ~3.4E+38
+
+    let float64Bit: f64;            //REM: double ( 15 to 16 precision) (8 bytes)
+                                    //REM: ~2.3E-308 to ~1.7E+308
+
+    let integer128Bit: i128;        //REM: signed 16 bytes integer
+                                    //REM: -170,141,183,460,469,231,731,687,303,715,884,105,727 to 170,141,183,460,469,231,731,687,303,715,884,105,727
+                                    //REM: UNDECILLION
+
+    let unsignedInteger128Bit: u128;//REM: unsigned 16 bytes integer
+                                    //REM: 0 to 340,282,366,920,938,463,463,374,607,431,768,211,455
+                                    //REM: UNDECILLION
+    
+    let signedSize: isize;          //REM: signed integer (architect dependent)
+                                    //REM: eithter i32 or i64 bits.
+
+    let unsignedSize: usize;        //REM: unsigned integer (architect dependent)
+                                    //REM: eithter u32 or u64 bits.
+
+    //REM: WRAPPER DATA TYPE
+    let charSequence: String;       //REM: 24 bytes (192 bits)
+    
     return Ok(());
 }
